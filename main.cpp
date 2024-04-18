b0d5ef11e90ee6b618fac411d05c0137a64d8f28
Nikita Krenov <nikitabaram@gmail.com>
2024-04-18 21:10:08 +0200

    This is the second practical task where the text and signature under it are written in white color

--- main.cpp
+++ main.cpp
@@ -7,14 +7,9 @@ int main() {
 	SetConsoleOutputCP(1251);
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	SetConsoleTextAttribute(hConsole, 0x06);
-	cout << "Hello World!" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
-	//#2
 	SetConsoleTextAttribute(hConsole, 0x0F);
 	cout << "Hello World!" << endl;
-	cout << "\\Dennis Ritchi\\" << endl;
+	cout << "\\\\Dennis Ritchi\\\\" << endl;
 
 	system("pause");
 	return 0;
