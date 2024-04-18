e0aa1c2370f57c9e066915ffb4fac8ae0fd83ee0
Nikita Krenov <nikitabaram@gmail.com>
2024-04-18 21:06:09 +0200

    This is the first task in practical work, in it we made the first yellow text and then the signature

--- main.cpp
+++ main.cpp
@@ -11,12 +11,7 @@ int main() {
 	cout << "Hello World!" << endl;
 	SetConsoleTextAttribute(hConsole, 0x0F);
 	cout << "/Bjarne Stroustrup/" << endl;
-	//#2
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "Hello World!" << endl;
-	cout << "\\Dennis Ritchi\\" << endl;
 
 	system("pause");
 	return 0;
-
 }
