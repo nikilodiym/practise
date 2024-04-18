7b7e76e012948bfd349e0048d8bfc2ab091ff42a
Nikita Krenov <nikitabaram@gmail.com>
2024-04-18 21:31:07 +0200

    This is the first homeword where writted my favorite song and autor

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	SetConsoleTextAttribute(hConsole, 0x06);
-	cout << "Hello World!" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
-	//#2
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "Hello World!" << endl;
-	cout << "\\Dennis Ritchi\\" << endl;
+	cout << "She says she's from the islands. And I'm just tryna find her. Girl you are my desire. Your all that I all require" << endl;
+	cout << "Vunes" << endl;
 
 	system("pause");
 	return 0;
