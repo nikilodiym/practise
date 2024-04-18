e9581cda183b7e94af684e877fd38d7995bcef5b
Nikita Krenov <nikitabaram@gmail.com>
2024-04-18 21:38:45 +0200

    This is the second homeWord where written text from poem "Every hunter wants..."

--- main.cpp
+++ main.cpp
@@ -7,14 +7,20 @@ int main() {
 	SetConsoleOutputCP(1251);
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	SetConsoleTextAttribute(hConsole, 0x06);
-	cout << "Hello World!" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
-	//#2
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "Hello World!" << endl;
-	cout << "\\Dennis Ritchi\\" << endl;
+	SetConsoleTextAttribute(hConsole, 0x04);
+	cout << "Every" << endl;
+	SetConsoleTextAttribute(hConsole, 0x0C);
+	cout << "\tHunter" << endl;
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "\t\tWants" << endl;
+	SetConsoleTextAttribute(hConsole, 0x0A);
+	cout << "\t\t\tTo Know" << endl;
+	SetConsoleTextAttribute(hConsole, 0x09);
+	cout << "\t\t\t\tWhere" << endl;
+	SetConsoleTextAttribute(hConsole, 0x01);
+	cout << "\t\t\t\t\tSits" << endl;
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cout << "\t\t\t\t\t\tPheasant" << endl;
 
 	system("pause");
 	return 0;
