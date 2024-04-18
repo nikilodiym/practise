b9d0866a2e407d31a8207161ec9d7ac87fc7725e
Nikita Krenov <nikitabaram@gmail.com>
2024-04-18 21:20:01 +0200

    This is the third practical task where a low of text is written in different colors

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@ int main() {
 	SetConsoleOutputCP(1251);
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "To be, ";
+	SetConsoleTextAttribute(hConsole, 0x04);
+	cout << "or not ";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "to be: that is the question: " << endl;
+	cout << "Whether ";
 	SetConsoleTextAttribute(hConsole, 0x06);
-	cout << "Hello World!" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
-	//#2
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "Hello World!" << endl;
-	cout << "\\Dennis Ritchi\\" << endl;
+	cout << "tis nobler in the mind to suffer. The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? " << endl;
+	cout << "William Shakespeare" << endl;
 
 	system("pause");
 	return 0;
-
 }
