a74890b5fda0d4ddd88965514ef9cf90662ba3dd
Nikita Krenov <nikitabaram@gmail.com>
2024-04-18 21:50:50 +0200

    This is the last homeWork for today so in this file can have maybe table with title about selling dogs

--- main.cpp
+++ main.cpp
@@ -6,15 +6,25 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
+	cout << "Продам цуценят." << endl;
+	cout << "Порода: ротвейлер." << endl;
+	cout << "……" << endl;
+	cout << "Тел.: 222-22-22" << endl;
+	cout << "222     |   ";
 	SetConsoleTextAttribute(hConsole, 0x06);
-	cout << "Hello World!" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
-	//#2
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "Hello World!" << endl;
-	cout << "\\Dennis Ritchi\\" << endl;
+	cout << "222  ";
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "|" << endl;
+	cout << "22      |   ";
+	SetConsoleTextAttribute(hConsole, 0x06);
+	cout << "22   ";
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "|" << endl;
+	cout << "22      |   ";
+	SetConsoleTextAttribute(hConsole, 0x06);
+	cout << "22   ";
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "|" << endl;
 
 	system("pause");
 	return 0;
